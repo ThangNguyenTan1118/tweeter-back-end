681eda2545116dc2ca84948198bf0677676e3f1c
ThangNguyenTan <thang.nguyentan@unified.com>
2021-12-13 09:56:34 +0700

    feat: users can now edit and delete posts

--- src/resolvers/posts.ts
+++ src/resolvers/posts.ts
@@ -6,9 +6,24 @@ import {
   Arg,
   Ctx,
   UseMiddleware,
+  InputType,
+  Field,
 } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
+import { UserInputError, ForbiddenError } from "apollo-server-express";
+
+@InputType()
+export class PostContentInput {
+  @Field(() => String)
+  title!: string;
+
+  @Field(() => String)
+  postText!: string;
+
+  @Field(() => String)
+  imageURL!: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -23,11 +38,11 @@ export class PostResolver {
   @Mutation(() => Post)
   @UseMiddleware(isAuth)
   async createPost(
-    @Arg("title") title: string,
-    @Arg("postText") postText: string,
-    @Arg("imageURL") imageURL: string,
+    @Arg("input") input: PostContentInput,
     @Ctx() { req }: MyContext
   ) {
+    const { title, postText, imageURL } = input;
+
     let post = new Post();
 
     post.title = title;
@@ -36,6 +51,56 @@ export class PostResolver {
     post.user = req.user;
 
     post = await post.save();
+
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
+  async updatePost(
+    @Arg("postId") postId: number,
+    @Arg("input") input: PostContentInput,
+    @Ctx() { req }: MyContext
+  ) {
+    const { title, postText, imageURL } = input;
+
+    let post = await Post.findOne({
+      id: postId,
+    });
+
+    if (!post) {
+      throw new UserInputError("The post with that ID does not exist");
+    }
+
+    if (post.user.id !== req.user.id) {
+      throw new ForbiddenError("You don't have permission to update this post");
+    }
+
+    post.title = title;
+    post.postText = postText;
+    post.imageURL = imageURL;
+
+    post = await post.save();
+
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
+  async deletePost(@Arg("postId") postId: number, @Ctx() { req }: MyContext) {
+    let post = await Post.findOne({
+      id: postId,
+    });
+
+    if (!post) {
+      throw new UserInputError("The post with that ID does not exist");
+    }
+
+    if (post.user.id !== req.user.id) {
+      throw new ForbiddenError("You don't have permission to delete this post");
+    }
+
+    await Post.delete(post);
 
     return post;
   }
